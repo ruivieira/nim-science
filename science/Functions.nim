6a0bba45d0fc75a295f178924a88e2866018ddab
Rui Vieira <ruidevieira@googlemail.com>
2019-05-07 09:23:05 +0100

    Change squared distance visibility

--- science/Functions.nim
+++ science/Functions.nim
@@ -1,6 +1,6 @@
 import math
 
-func squared_distance(x: seq[float], y: seq[float]): float =
+func squared_distance*(x: seq[float], y: seq[float]): float =
     var sum = 0.0
     for i in 0..<len(x):
         sum += (x[i]-y[i]).pow(2.0)
